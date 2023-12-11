3ada35386fd02e445be64074b67b7b8c25a40e2b
Patrik Michal Vlcek <patrik.vlcek@ysoft.com>
2023-12-11 10:58:02 +0100

    feat: added lsp and remaps

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,9 +1,10 @@
-require('rose-pine').setup({
+require('gruvbox').setup({
     disable_background = true
 })
 
 function ColorMyPencils(color)
-	color = color or "rose-pine"
+	color = color or "gruvbox"
+    vim.o.background = "dark"
 	vim.cmd.colorscheme(color)
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
--- lua/wolf/packer.lua
+++ lua/wolf/packer.lua
@@ -22,8 +22,26 @@ use {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
     requires = { {"nvim-lua/plenary.nvim"} }
-}use('mbbill/undotree')
+}
+use('mbbill/undotree')
 use('tpope/vim-fugitive')
+use {
+  'VonHeikemen/lsp-zero.nvim',
+  branch = 'v3.x',
+  requires = {
+    --- Uncomment these if you want to manage LSP servers from neovim
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
+
+    -- LSP Support
+    {'neovim/nvim-lspconfig'},
+    -- Autocompletion
+    {'hrsh7th/nvim-cmp'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'L3MON4D3/LuaSnip'},
+  }
+}
+use("ellisonleao/gruvbox.nvim")
 
 end)
 
